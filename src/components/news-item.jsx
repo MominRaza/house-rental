26e06cef0f8f92899d7a95acfafdd6d41c93de34
MominRaza <mominr082@gmail.com>
2021-06-26 04:13:26 +0530

    added news model

--- src/components/news-item.jsx
+++ src/components/news-item.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import news1 from "../images/news1.jpg";
-
 class NewsItem extends Component {
   state = {};
   render() {
+    const news = this.props.news;
     return (
-      <Link to="#" className="flex-item card">
-        <img src={news1} alt="The pros and cons of buying agricultural land" />
-        <h3>The pros and cons of buying agricultural land</h3>
-        <p>
-          A number of urban investors are investing in agricultural land, owing
-          to the slump in urban realty and the promise of greater long-term
-          returns. We examine the benefits and risks, for those who wish to buy
-          agricultural land.
-        </p>
+      <Link to={"/news/" + news["id"]} className="flex-item card">
+        <img src={news["imageUrl"]} alt={news["title"]} />
+        <h3>{news["title"]}</h3>
+        <p>{news["details"]}</p>
       </Link>
     );
   }
--- src/screens/home/components/newsSection.jsx
+++ src/screens/home/components/newsSection.jsx
@@ -3,7 +3,58 @@ import React, { Component } from "react";
 import NewsItem from "../../../components/news-item";
 
 class NewsSection extends Component {
-  state = {};
+  state = {
+    news: [
+      {
+        id: 1,
+        title: "The pros and cons of buying agricultural land 1",
+        details:
+          "A number of urban investors are investing in agricultural land, owing to the slump in urban realty and the promise of greater long-term returns. We examine the benefits and risks, for those who wish to buy agricultural land.",
+        news: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates vitae error, incidunt earum et eaque, culpa tempore fugiat debitis quibusdam vero magni, dicta ipsum harum omnis deleniti ad? Nemo voluptate magni neque dolorum perferendis necessitatibus laboriosam nam facere provident quos deleniti quas beatae qui est dolore ipsum, expedita ducimus.",
+        imageUrl: "images/news1.jpg",
+      },
+      {
+        id: 2,
+        title: "The pros and cons of buying agricultural land 2",
+        details:
+          "A number of urban investors are investing in agricultural land, owing to the slump in urban realty and the promise of greater long-term returns. We examine the benefits and risks, for those who wish to buy agricultural land.",
+        news: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates vitae error, incidunt earum et eaque, culpa tempore fugiat debitis quibusdam vero magni, dicta ipsum harum omnis deleniti ad? Nemo voluptate magni neque dolorum perferendis necessitatibus laboriosam nam facere provident quos deleniti quas beatae qui est dolore ipsum, expedita ducimus.",
+        imageUrl: "images/news2.jpg",
+      },
+      {
+        id: 3,
+        title: "The pros and cons of buying agricultural land 3",
+        details:
+          "A number of urban investors are investing in agricultural land, owing to the slump in urban realty and the promise of greater long-term returns. We examine the benefits and risks, for those who wish to buy agricultural land.",
+        news: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates vitae error, incidunt earum et eaque, culpa tempore fugiat debitis quibusdam vero magni, dicta ipsum harum omnis deleniti ad? Nemo voluptate magni neque dolorum perferendis necessitatibus laboriosam nam facere provident quos deleniti quas beatae qui est dolore ipsum, expedita ducimus.",
+        imageUrl: "images/news3.jpg",
+      },
+      {
+        id: 4,
+        title: "The pros and cons of buying agricultural land 4",
+        details:
+          "A number of urban investors are investing in agricultural land, owing to the slump in urban realty and the promise of greater long-term returns. We examine the benefits and risks, for those who wish to buy agricultural land.",
+        news: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates vitae error, incidunt earum et eaque, culpa tempore fugiat debitis quibusdam vero magni, dicta ipsum harum omnis deleniti ad? Nemo voluptate magni neque dolorum perferendis necessitatibus laboriosam nam facere provident quos deleniti quas beatae qui est dolore ipsum, expedita ducimus.",
+        imageUrl: "images/news4.jpg",
+      },
+      {
+        id: 5,
+        title: "The pros and cons of buying agricultural land 5",
+        details:
+          "A number of urban investors are investing in agricultural land, owing to the slump in urban realty and the promise of greater long-term returns. We examine the benefits and risks, for those who wish to buy agricultural land.",
+        news: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates vitae error, incidunt earum et eaque, culpa tempore fugiat debitis quibusdam vero magni, dicta ipsum harum omnis deleniti ad? Nemo voluptate magni neque dolorum perferendis necessitatibus laboriosam nam facere provident quos deleniti quas beatae qui est dolore ipsum, expedita ducimus.",
+        imageUrl: "images/news5.jpg",
+      },
+      {
+        id: 6,
+        title: "The pros and cons of buying agricultural land 6",
+        details:
+          "A number of urban investors are investing in agricultural land, owing to the slump in urban realty and the promise of greater long-term returns. We examine the benefits and risks, for those who wish to buy agricultural land.",
+        news: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates vitae error, incidunt earum et eaque, culpa tempore fugiat debitis quibusdam vero magni, dicta ipsum harum omnis deleniti ad? Nemo voluptate magni neque dolorum perferendis necessitatibus laboriosam nam facere provident quos deleniti quas beatae qui est dolore ipsum, expedita ducimus.",
+        imageUrl: "images/news6.jpg",
+      },
+    ],
+  };
   render() {
     return (
       <section className="news">
@@ -11,13 +62,9 @@ class NewsSection extends Component {
           <h2 className="h4">News &amp; Articles</h2>
           <p>Read what's happening in Real Estate</p>
           <div className="flex">
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
+            {this.state.news.map((newsItem) => (
+              <NewsItem news={newsItem} />
+            ))}
           </div>
         </div>
       </section>
