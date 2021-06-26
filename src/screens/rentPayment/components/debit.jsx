cacd12268f2a2133430f4532061a7a0316f96f21
MominRaza <mominr082@gmail.com>
2021-06-27 02:42:07 +0530

    added receiver details

--- src/screens/rentPayment/components/debit.jsx
+++ src/screens/rentPayment/components/debit.jsx
@@ -7,6 +7,7 @@ class Debit extends Component {
       <div className="card payment">
         <h2 className="h4">Debit Card Details</h2>
         <form action="#">
+          <p>Sender details:</p>
           <label>
             Name on card:
             <input type="text" name="name" />
@@ -28,6 +29,23 @@ class Debit extends Component {
           <label>
             CVV:
             <input type="text" name="cvv" />
+          </label>
+          <p>Receiver details:</p>
+          <label>
+            Bank name:
+            <input type="text" name="bank-name" />
+          </label>
+          <label>
+            Account holder name:
+            <input type="text" name="holder-name" />
+          </label>
+          <label>
+            Account number:
+            <input type="text" name="account-number" />
+          </label>
+          <label>
+            IFSC code:
+            <input type="text" name="ifsc-code" />
           </label>
           <input
             type="submit"
--- src/screens/rentPayment/components/upi.jsx
+++ src/screens/rentPayment/components/upi.jsx
@@ -7,9 +7,27 @@ class Upi extends Component {
       <div className="card payment">
         <h2 className="h4">UPI Details</h2>
         <form action="#">
+          <p>Sender details:</p>
           <label>
             UPI Id:
             <input type="text" name="upi-id" />
+          </label>
+          <p>Receiver details:</p>
+          <label>
+            Bank name:
+            <input type="text" name="bank-name" />
+          </label>
+          <label>
+            Account holder name:
+            <input type="text" name="holder-name" />
+          </label>
+          <label>
+            Account number:
+            <input type="text" name="account-number" />
+          </label>
+          <label>
+            IFSC code:
+            <input type="text" name="ifsc-code" />
           </label>
           <input
             type="submit"
