180415d501d2dbc7e6d76f1336d2912b0203ed02
MominRaza <mominr082@gmail.com>
2021-06-27 03:22:10 +0530

    state to props

--- src/screens/news.jsx
+++ src/screens/news.jsx
@@ -9,7 +9,7 @@ class News extends Component {
       <section className="news-page">
         <h1 className="h3">News Segment</h1>
         <p className="b1">Read things happening in Real Estate</p>
-        {this.state.news.map((newsItem) => (
+        {this.props.news.map((newsItem) => (
           <div className="list-item">
             <NewsItem news={newsItem} />
           </div>
