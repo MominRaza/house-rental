c83567a0802bbe1d676e80dff9b114943c2a725b
amardeep <amardeepojha48@gmail.com>
2021-06-27 04:45:42 +0530

    Featured section image changed

--- src/screens/home/components/featuredSection.jsx
+++ src/screens/home/components/featuredSection.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import ready from "../../../images/ready.webp";
-import townships from "../../../images/township.webp";
-import luxury from "../../../images/luxury.webp";
-import affordable from "../../../images/affordable.webp";
+import ready from "../../../images/ready.jpg";
+import townships from "../../../images/hotels.jpg";
+import luxury from "../../../images/farmhouse.jpg";
+import affordable from "../../../images/affordable.jpg";
 import hour from "../../../images/hour.jpg";
 
 class FeaturedSection extends Component {
@@ -16,32 +16,32 @@ class FeaturedSection extends Component {
           <h2 className="h4">Featured Collections</h2>
           <p>Handpicked projects for you</p>
           <div className="flex">
-            <Link to="/search?condition=move-in" className="flex-item card">
-              <div>
-                <img src={ready} alt="Ready to Move-In" />
-                <h3>Ready to Move-In</h3>
-                <p>Comfortable homes available for immediate use</p>
-              </div>
-            </Link>
-            <Link to="/search?type=township" className="flex-item card">
-              <div>
-                <img src={townships} alt="Townships" />
-                <h3>Townships</h3>
-                <p>Redefining community living</p>
-              </div>
-            </Link>
-            <Link to="/search?price=luxury" className="flex-item card">
-              <div>
-                <img src={luxury} alt="Luxury" />
-                <h3>Luxury</h3>
-                <p>Premium housing for the lifestyle-conscious</p>
-              </div>
-            </Link>
             <Link to="/search?type=hour" className="flex-item card">
               <div>
                 <img src={hour} alt="Hour Ready" />
                 <h3>Hour Ready</h3>
                 <p>Enjoy perks of metered time.</p>
+              </div>
+            </Link>
+            <Link to="/search?condition=move-in" className="flex-item card">
+              <div>
+                <img src={ready} alt="Ready to Move-In" />
+                <h3>Ready to Move-In</h3>
+                <p>Homes ready to use</p>
+              </div>
+            </Link>
+            <Link to="/search?type=township" className="flex-item card">
+              <div>
+                <img src={townships} alt="Hotels" />
+                <h3>Hotels</h3>
+                <p>Relaxation like never before</p>
+              </div>
+            </Link>
+            <Link to="/search?price=luxury" className="flex-item card">
+              <div>
+                <img src={luxury} alt="Farmhouse" />
+                <h3>Farmhouse</h3>
+                <p>Excellent places to spend weekends</p>
               </div>
             </Link>
             <Link to="/search?price=affordable" className="flex-item card">
