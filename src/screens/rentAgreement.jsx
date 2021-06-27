0d6158c0f45f2428caf38cbcb3fd6cf0fc4426b1
MominRaza <mominr082@gmail.com>
2021-06-28 00:31:25 +0530

    added rent agreement

--- src/screens/rentAgreement.jsx
+++ src/screens/rentAgreement.jsx
@@ -3,7 +3,49 @@ import React, { Component } from "react";
 class RentAgreement extends Component {
   state = {};
   render() {
-    return <h1 className="h3">Rent Agreement</h1>;
+    return (
+      <section className="tenant-verify center">
+        <h1 className="h3">Rent Agreement</h1>
+        <form action="#" className="card left">
+          <label>
+            Full Name:
+            <input type="text" name="full-name" />
+          </label>
+          <label>
+            Phone Number:
+            <input type="text" name="phone-number" />
+          </label>
+          <label>
+            Email Id:
+            <input type="email" name="email" />
+          </label>
+          <p>I am a</p>
+          <div className="flex">
+            <label>
+              <input type="radio" name="iam" />
+              Tenant
+            </label>
+            <label>
+              <input type="radio" name="iam" />
+              Landlord
+            </label>
+          </div>
+          <label>
+            State:
+            <input type="text" name="state" />
+          </label>
+          <label>
+            City:
+            <input type="text" name="city" />
+          </label>
+          <input
+            type="submit"
+            value="Pay Rs. 100 &amp; Verify"
+            className="btn primary"
+          />
+        </form>
+      </section>
+    );
   }
 }
 
