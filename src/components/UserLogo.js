347ae2188b95620a74901fe725fee834b7ae9bc4
MominRaza <mominr082@gmail.com>
2021-06-30 01:05:39 +0530

    show user icon if login instead of login

--- src/components/UserLogo.js
+++ src/components/UserLogo.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/AuthContext";
 
 import Login from "./login";
 
 export default function UserLogo() {
+  const { currentUser } = useAuth();
   const [loginModal, setLoginModal] = useState(false);
 
   function toggleLogin() {
     setLoginModal(!loginModal);
   }
-  return (
+  return currentUser ? (
+    <Link to="/user" className="btn btn-o white rd-circle">
+      <i className="material-icons">person</i>
+    </Link>
+  ) : (
     <>
       <Link to="#" className="btn btn-t white sm icon-l" onClick={toggleLogin}>
         <i className="material-icons md-18">person_outline</i>
